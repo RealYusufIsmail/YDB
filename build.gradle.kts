e0946279cd50c29a2d70ba2827148e64db3cdf91
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-10-29 11:09:03 +0100

    getting started

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.20"
+    id("com.diffplug.spotless") version "6.11.0"
 }
 
 group = "io.github.realyusufismail"
+
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,14 +15,48 @@ repositories {
 }
 
 dependencies {
-    implementation("io.github.realyusufismail:ydwk:0.0.3-SNAPSHOT")
+    implementation("io.github.realyusufismail:ydwk:0.0.4-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.test { useJUnitPlatform() }
+
+configurations { all { exclude(group = "org.slf4j", module = "slf4j-log4j12") } }
+
+spotless {
+    kotlin {
+        // Excludes build folder since it contains generated java classes.
+        targetExclude("build/**")
+        ktfmt("0.39").dropboxStyle()
+
+        licenseHeader(
+            """/*
+ * Copyright 2022 RealYusufIsmail.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ """)
+    }
+
+    kotlinGradle {
+        target("**/*.gradle.kts")
+        ktfmt("0.39").dropboxStyle()
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "17" }
---
+++ src/main/kotlin/io/github/realyusufismail/YDB.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2022 RealYusufIsmail.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *
+ * you may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+package io.github.realyusufismail
+
+import io.github.realyusufismail.jconfig.classes.JConfigBuilder
+import io.github.realyusufismail.jconfig.classes.JConfigException
+import io.github.ydwk.ydwk.BotBuilder
+import io.github.ydwk.ydwk.YDWK
+import io.github.ydwk.ydwk.evm.backend.event.on
+import io.github.ydwk.ydwk.evm.event.events.gateway.ReadyEvent
+import org.slf4j.LoggerFactory
+
+val config = JConfigBuilder().build()
+
+fun main() {
+    val logger = LoggerFactory.getLogger("YDB")
+
+    val ydkw: YDWK =
+        BotBuilder.createDefaultBot(
+                config["token"]?.asString ?: throw JConfigException("Token not found"))
+            .build()
+
+    ydkw.on<ReadyEvent> {
+        logger.info("Logged in as ${it.ydwk.bot?.name}")
+    }
+}
